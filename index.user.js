be56739e0e12b000c954f3adcbe7d9e266780223
A̴m̴r̴ ع̲مِےـرۅ <akkk33@protonmail.com>
2023-04-18 07:08:56 +0200

    patch

--- index.user.js
+++ index.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Noor-book direct download - تحميل مباشر من مكتبة نور
 // @namespace    a3k
-// @version      1.0
+// @version      1.0.1
 // @description  هذا البرنامج النصي يتخطي المؤقت التنازلي ويسمح بالتحميل المباشر دون انتظار
 // @description:disclaimer This script is intended for personal use only. The author is not responsible for any misuse of the tool or any legal issues that may arise from its use.
 // @description:disclaimer هذا الملف النصي كِتب لغرض التعليم فقط والمؤلف لا يتحمل المسئولية القانونية الناتجة عن إساءة استخدامه.
 // @author       a3k
-// @match        https://www.noor-book.com/*pdf
+// @match        https://www.noor-book.com/.*
 // @icon         https://www.noor-book.com/publice/icons/favicon-96x96.png
 // @license            GPL-3.0-only
 // @grant              none
@@ -18,7 +18,10 @@
 
   // Find the download link and click it
   const modalLink = document.querySelector('#download_circle>a');
-  modalLink.addEventListener('click', function() {
+  if (!modalLink) {
+    return;
+  }
+  modalLink.addEventListener('click', function () {
     window.go_gownload();
     setTimeout(function () {
       const downloadLink = document.querySelectorAll('.internal_download_link')[0];
