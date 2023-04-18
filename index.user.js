4415accfb3efe2c7c632bc5021fdd446849b3dca
A̴m̴r̴ ع̲مِےـرۅ <akkk33@protonmail.com>
2023-04-18 07:30:02 +0200

    .

--- index.user.js
+++ index.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         Noor-book direct download - تحميل مباشر من مكتبة نور
-// @namespace    amr3k
-// @version      1.0
+// @name        Noor-book direct download - تحميل مباشر من مكتبة نور
+// @namespace   a3k
+// @version     1.0
 // @description  هذا البرنامج النصي يتخطي المؤقت التنازلي ويسمح بالتحميل المباشر دون انتظار
 // @description:disclaimer This script is intended for personal use only. The author is not responsible for any misuse of the tool or any legal issues that may arise from its use.
-// @author       amr3k - ع̲مِےـرۅ
-// @match        https://www.noor-book.com/*
-// @icon         https://www.noor-book.com/publice/icons/favicon-96x96.png
-// @license            MIT
-// @grant              none
-// @run-at             document-end
+// @description:disclaimer-ar هذا البرنامج النصي مكتوب لغرض تعليمي، ولا تقع على المؤلف المسئولية القانونية النابعة من الاستخدام الخاطيء لهذا البرنامج
+// @author      a3k
+// @match       https://www.noor-book.com/*
+// @icon        https://www.noor-book.com/publice/icons/favicon-96x96.png
+// @license     GPL-3.0-only
+// @grant       none
+// @run-at      document-end
 // ==/UserScript==
 
 (function () {
